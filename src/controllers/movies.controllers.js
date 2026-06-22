21bb40f2065489dda4fe46aad257c2526fd7c4f3
oscar <oschneider742@gmail.com>
2026-06-22 20:14:16 -0300

    feat: se creo la funcion para cargar una pelicula

--- src/controllers/movies.controllers.js
+++ src/controllers/movies.controllers.js
@@ -2,8 +2,59 @@ import { Movie } from "../models/movies.models.js";
 
 
 export const getAllMovies = (req,res) =>{
-    const movies = Movie.findAll();
+    try{
+        const movies = Movie.findAll();
 
-    res.json(movies);
+        res.json(movies);
+    }catch(err){
+        res.status(500).json({message:'Se produjo el error' , error: err.message });
+    }
 }
 
+export const createMovie = (req, res) => {
+    try {
+        const { title, gender, duration, year } = req.body;
+        let synopsis = '';
+        
+        if (title === "" || gender === "" || duration === "" || year === "") {
+            res.status(404).json({
+                message:"No puede haver campos vacios"
+            })
+        }
+        else {
+            if (duration >0 && typeof duration != 'string' && Number.isInteger(duration)) {
+                
+                if (year > 1888 && year < 9999) {
+
+                    if (req.body.synopsis != '') {
+                        if (typeof req.body.synopsis == 'string') {
+                            synopsis = req.body.synopsis
+                        }
+                    }
+                    
+                    const newMovie = [
+                        title,
+                        gender,
+                        duration,
+                        year,
+                        synopsis
+                    ]
+
+                    res.status(201).json({message:"Se subio una pelicula" ,newMovie});
+
+                } else {
+                    res.status(404).json({
+                        message:"No puede haber peliculas antes de 1888"
+                    })
+                }
+            } else {
+                res.status(404).json({
+                    message:"La pelicula debe tener una duracion minima de un minuto"
+                })
+            }
+            
+        }
+    } catch (err) {
+        res.status(500).json({ message: 'Se produjo el error', error: err.message });
+    }
+};
